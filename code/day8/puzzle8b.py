24727e866e4e052003f4790f494e84fffc97edd5
Jarek Brown <31749343+JarekBrown@users.noreply.github.com>
2022-12-08 23:17:34 -0700

    still no success

--- code/day8/puzzle8b.py
+++ code/day8/puzzle8b.py
@@ -18,8 +18,7 @@ def read_file(filename):
     file.close()
     return [i.strip('\n') for i in data]
 
-def main():
-    data = read_file('input.txt')
+def scenic(data):
     tmp = []
     for line in data:
         tmp.append([int(i) for i in line])
@@ -28,10 +27,12 @@ def main():
     j = 0
     col = np.rot90(lines)
     col = col[::-1]
+    scores = []
+    # print(lines)
     for line in lines:
         n = len(line)
         for tree in range(n):  
-            # print(line,tree) 
+            # print(tree) 
             # print(col)
             # print(col[(tree)],j) 
             right = 0
@@ -41,6 +42,7 @@ def main():
             pos = [line[tree-1::],line[tree+1:], col[tree][j-1::], col[tree][j+1:]]
             # print(pos[0],pos[1],pos[2],pos[3])
             for i in pos[0]:
+                # print(i)
                 if(i >= line[tree]):
                     break
                 left += 1
@@ -69,9 +71,10 @@ def main():
                     # break
             # print('down',down)
             score = left*right*up*down
+            scores.append(score)
             # print(score,right,left,up,down, max_score)
-            if score > max_score:
-                max_score = score
+            # if score >= max_score:
+            #     max_score = score
         #     # print(i)
         #     if tree == 0 or tree == n-1 or i == 0 or i == n-1:
         #         vision += 1                      
@@ -84,7 +87,13 @@ def main():
         #         # print(col, i)
         j += 1
         # j = 0
-    print('max score',max_score)#+(2*lines.shape[0])-4)
+    print(j)
+    return(np.max(scores))
+
+def main():
+    data = read_file('input.txt')
+    print(scenic(data))
+    
 
 
 if __name__ == "__main__":
