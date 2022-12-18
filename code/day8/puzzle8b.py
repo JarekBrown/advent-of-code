a45ded5683cc360f7ac2c5070d1ab4b65ddae321
Jarek Brown <31749343+JarekBrown@users.noreply.github.com>
2022-12-18 14:22:31 -0700

    day 8 finally solved

--- code/day8/puzzle8b.py
+++ code/day8/puzzle8b.py
@@ -23,76 +23,47 @@ def scenic(data):
     for line in data:
         tmp.append([int(i) for i in line])
     lines = np.asarray(tmp)
-    max_score = 1
     j = 0
     col = np.rot90(lines)
     col = col[::-1]
     scores = []
-    # print(lines)
     for line in lines:
         n = len(line)
         for tree in range(n):  
-            # print(tree) 
-            # print(col)
-            # print(col[(tree)],j) 
-            right = 0
-            left = 0
-            up = 0
-            down = 0
-            pos = [line[tree-1::],line[tree+1:], col[tree][j-1::], col[tree][j+1:]]
-            # print(pos[0],pos[1],pos[2],pos[3])
-            for i in pos[0]:
-                # print(i)
-                if(i >= line[tree]):
-                    break
-                left += 1
-                    # break
-                # print('left:',left)
-            for i in pos[1]:
-                if(i >= line[tree]):
-                    break
-                right += 1
-                    # break
-                # print('right :',right)
-            for i in pos[2]:
-                if(i >= line[tree]):
-                    break
-                up +=1
-                # print('up:',up)
-                    # break
-            for i in pos[3]:                
-                # print(col[j-tree][i], line[tree])
-                # print(i)
-                if(i >= line[tree]):
-                    break
-                    # print('yes')
-                down +=1
-                # print('down:',down)
-                    # break
-            # print('down',down)
-            score = left*right*up*down
+            if j == 0 or j == n-1 or tree == 0 or tree == n-1:
+                score = 0
+            else: 
+                right = 0
+                left = 0
+                up = 0
+                down = 0
+                pos = [line[:tree],line[tree+1:], col[tree][:j], col[tree][j+1:]]
+                for i in np.flip(pos[0]):
+                    left += 1
+                    if(i >= line[tree]):
+                        break
+                for i in pos[1]:
+                    right += 1
+                    if(i >= line[tree]):
+                        break
+                for i in np.flip(pos[2]):
+                    up +=1
+                    if(i >= line[tree]):
+                        break
+                for i in pos[3]:     
+                    down +=1
+                    if(i >= line[tree]):
+                        break
+                score = left*right*up*down
             scores.append(score)
-            # print(score,right,left,up,down, max_score)
-            # if score >= max_score:
-            #     max_score = score
-        #     # print(i)
-        #     if tree == 0 or tree == n-1 or i == 0 or i == n-1:
-        #         vision += 1                      
-        #     elif (all(line[tree] > line[:tree]) or all(line[tree] > line[tree+1:])):
-        #         vision += 1
-        #         # print('row:',line, tree)
-        #     elif (all(line[tree] > col[:i]) or all(line[tree] > col[i+1:])):
-        #         vision += 1
-        #         # print('col:',line, col, tree,i)
-        #         # print(col, i)
         j += 1
-        # j = 0
-    print(j)
     return(np.max(scores))
 
 def main():
+    data = read_file('test.txt')
+    print('test file: ', scenic(data))
     data = read_file('input.txt')
-    print(scenic(data))
+    print('input file: ', scenic(data))
     
 
 
