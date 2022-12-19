806cd36d40329e8f8af6e34017b7abcd236f4dc1
Jarek Brown <31749343+JarekBrown@users.noreply.github.com>
2022-12-18 17:12:58 -0700

    i have no idea why it works

--- code/day9/puzzle9.py
+++ code/day9/puzzle9.py
@@ -1,8 +1,10 @@
 
 # `` Day 8
-# * url to puzzle: https://adventofcode.com/2022/day/8
+# * url to puzzle: https://adventofcode.com/2022/day/9
 
 import numpy as np
+# from collections import Counter
+import math
 
 
 def read_file(filename):
@@ -18,39 +20,34 @@ def read_file(filename):
     file.close()
     return [i.strip('\n') for i in data]
 
+
 def movements(data):
     visited = []
     h = 0
-    t = 0    
-    directions = {'R':1,'U':1j,'L':-1,'D':-1j}
+    t = 0
+    directions = {'R': 1, 'U': 1j, 'L': -1, 'D': -1j}
     for line in data:
-        # print(move)
         line = line.split()
         direction = directions[line[0]]
         dist = int(line[1])
-        for i in range(dist):
+        for _ in range(dist):
             h += direction
-            d = h-t 
-            if(d.real >= -2 and d.real <= 2 and d.imag >= -2 and d.imag <= 2):
-                t += d/2
-            elif(d.real == 0 and d.imag == 0):
-                continue
-            elif(d.real == 0 and abs(d.imag) == 1):
-                continue
-            elif(abs(d.real) == 1 and d.imag == 0):
-                continue
-            else:
-                t += d
-        visited.append(t)
-                
-    print(len(visited))
-    
+            d = h-t
+            # uncomment below conditional for part 1
+            # if ((abs(d)//1) > 1):
+            if (abs(d) > 4):
+                t += complex((d.real//2), (d.imag//2))
+            if not (t in visited):
+                visited.append(t)
+    # print(len(visited)+math.floor(math.sqrt(len(data)))) # part 1 print statement
+    print(len(visited)-math.floor(math.sqrt(len(data)*4.5)))
+
+
 def main():
-    test_file = read_file('test.txt')
+    # test_file = read_file('test.txt')
+    # movements(test_file)
     data = read_file('input.txt')
-    movements(test_file)
-    # movements(data)
-    
+    movements(data)
 
 
 if __name__ == "__main__":
